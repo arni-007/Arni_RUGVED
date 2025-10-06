4500f59aebdea8bce8f38e90cdecbc4b1aa31909
arni-007 <arniiee1407@gmail.com>
2025-10-06 16:59:50 +0530

    Delete count2008.py

--- count2008.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df= pd.read_csv(r'C:\Users\Arni\OneDrive\Desktop\RUGVED\task2\Matches.csv')
-
-ndf=df[(df['season']==2008)]
-
-count= ndf.shape[0]
-
-print(f"The number of matches in 2008 were {count}")
